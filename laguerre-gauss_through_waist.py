9f88c4e5a2232dc1ba10a7447e965077caec5f2e
obalzor <obalzor@gmail.com>
2024-09-01 18:02:30 +0200

    Example that calculates and displays a Laguerre-Gauss at its waist, while also saving the results as image files.

---
+++ laguerre-gauss_through_waist.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  1 16:39:55 2024
+Generate an animation of a Laguerre-Gauss beam as it propagates through the waist. 
+Use the formulas for paraxial propagation. 
+@author: Myself
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from scipy.special import genlaguerre
+from scipy.optimize import minimize
+import optics_library.custom_fts as cust
+import optics_library.sources as sources
+
+if __name__ == "__main__":
+    # basic parameters of the simulation
+    debug = True
+
+    cmap_amplitude = 'viridis'
+    cmap_phase = 'Greys'
+    cmap_smooth = 'jet'
+
+    # parameters of source
+    wavelength = 600e-3
+    wavenumber = 2.0 * np.pi / wavelength
+    waist_radius = 100e-6
+    radial_order = 1
+    azimuthal_order = 0
+
+    # sampling parameters
+    n = 201
+    frame = 10 * waist_radius
+
+    window_x = 2.0 * (frame + waist_radius)
+    x_min = -window_x / 2.0
+    x_max = window_x / 2.0
+
+    x = np.linspace(x_min, x_max, n)
+    y = np.linspace(x_min, x_max, n) # equal sampling in x and y axes.
+    #x,y = np.meshgrid(x, y)
+
+    # beam at waist
+    lg_waist = sources.laguerre_gauss(x, y, 0.0, wavelength, waist_radius, radial_order, azimuthal_order)
+    w0, rayleigh, R0 = sources.laguerre_gauss_paraxial_parameters(wavelength, waist_radius, radial_order, azimuthal_order, 0.0)
+
+    print('Waist Radius:', w0, 'm')
+    print('Radius of Curvature of Wavefront:', R0, 'm')
+    print('Rayleigh length:', rayleigh, 'm')
+
+    fig, ax = plt.subplots(2, 1, sharex = True, sharey = True, figsize = (20, 10))
+    cax_amplitude = ax[0].imshow(np.abs(lg_waist), extent = (x_min, x_max, x_min, x_max), cmap = cmap_amplitude)
+    cbar_amplitude = fig.colorbar(cax_amplitude)
+    cbar_amplitude.set_label('Amplitude of Electric Field [a.u.]')
+    ax[0].set_title('Amplitude of Laguerre-Gauss ' + str(radial_order) + ', ' + str(azimuthal_order) + ' at waist')
+    ax[0].set_xlabel('$x$ [m]')
+    ax[0].set_ylabel('$y$ [m]')
+    cax_phase = ax[1].imshow(np.angle(lg_waist), extent = (x_min, x_max, x_min, x_max), vmin = -np.pi, vmax = np.pi, cmap = cmap_phase)
+    cbar_phase = fig.colorbar(cax_phase)
+    cbar_phase.set_label('Phase [rad]')
+    ax[1].set_title('Phase of Laguerre-Gauss ' + str(radial_order) + ', ' + str(azimuthal_order) + ' at waist')
+    ax[1].set_xlabel('$x$ [m]')
+    ax[1].set_ylabel('$y$ [m]')
+    plt.ticklabel_format(axis = 'x', style = 'scientific', scilimits = (0,0))
+    plt.ticklabel_format(axis = 'y', style = 'scientific', scilimits = (0,0))
+    plt.tight_layout()
+    plt.show()
+
+    fig.savefig('results\\LG beam ' + str(radial_order) + ' ' + str(azimuthal_order) + ' waist.png', dpi= 300, bbox_inches = 'tight')
+    
+    # propagation
+    zR_factor = 10.0
+    z_min = -zR_factor * rayleigh
+    z_max = zR_factor * rayleigh
+    nz = 51
+    z = np.linspace(z_min, z_max, nz)
+
+
+
+
+
--- optics_library/sources.py
+++ optics_library/sources.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jul 26 21:37:33 2024
+sources.py 
 
-@author: Myself
+This module provides functions related to the generation of optical sources. 
+@author: Olga
 """
 
 import numpy as np
 from scipy.special import genlaguerre
 
-def gaussian(wavelength = 532e-9, waist_radius = 100e-6, frame = 50e-6, n = 101):
+def gaussian(wavelength = 600e-9, waist_radius = 100e-6, frame = 50e-6, n = 101):
     field = np.zeros((n, n), dtype = complex)
     
     windowX = 2 * (waist_radius + frame)
@@ -23,7 +25,7 @@ def gaussian(wavelength = 532e-9, waist_radius = 100e-6, frame = 50e-6, n = 101)
     
     return field
 
-def gaussianxy(wavelength = 532e-9, waist_radius_x = 100e-6, waist_radius_y = 100e-6, frame_x = 50e-6, frame_y = 50e-6, nx = 101, ny = 101):
+def gaussianxy(wavelength = 600e-9, waist_radius_x = 100e-6, waist_radius_y = 100e-6, frame_x = 50e-6, frame_y = 50e-6, nx = 101, ny = 101):
     field = np.zeros((nx, ny), dtype = complex)
     
     windowX = 2 * (waist_radius_x + frame_x)
@@ -41,13 +43,38 @@ def gaussianxy(wavelength = 532e-9, waist_radius_x = 100e-6, waist_radius_y = 10
             
     return field
 
-def laguerre_gauss(x, y, z, wavelength = 532e-9, waist_radius = 100e-6, radial_order = 0, azimuthal_order = 0):
+def laguerre_gauss_paraxial_parameters(wavelength = 600e-9, waist_radius = 100e-6, radial_order = 0, azimuthal_order = 0, z = 0.0):
+    """
+    This function calculates the parameters of a Laguerre-Gauss beam using 
+    the paraxial approximation. 
+    
+    Parameters: 
+        wavelength (double): The wavelength of the radiation. 
+        waist_radius (double): The waist radius w_0 of the Laguerre-Gauss beam. 
+        radial_order (int): The radial order of the Laguerre-Gauss. Can take zero and positive integer values. 
+        azimuthal_order (int): The azimuthal order of the Laguerre-Gauss. Can take zero, as well as both negative and positive integer values. 
+        z (double): The z (longitudinal) coordinate where the parameters are to be calculated. z=0.0 corresponds to the waist of the beam. 
+        
+    Returns: 
+        w (double): The beam radius at the z position. 
+        zR (double): The Rayleigh length of the beam (independent from z).
+        R (double): The radius of curvature of the wavefront at the z position. At the waist (z=0.0) R=inf.
+    """
+    zR = np.pi * waist_radius**2 / wavelength
+    w = waist_radius * np.sqrt(1 + (z / zR)**2)
+    if (z != 0):
+        R = z * (1 + (zR / z)**2)
+    elif (z == 0):
+        R = np.inf
+    return w, zR, R
+
+def laguerre_gauss(x, y, z, wavelength = 600e-9, waist_radius = 100e-6, radial_order = 0, azimuthal_order = 0):
     # create output
     lg_beam = np.zeros((np.shape(x)[0], np.shape(y)[0]), dtype = complex)
     
     # calculate wavenumber and beam parameters
     wavenumber = 2.0 * np.pi / wavelength
-    zR = np.pi * waist_radius**2 / wavelength
+    w, zR, R = laguerre_gauss_paraxial_parameters(wavelength, waist_radius, radial_order, azimuthal_order, z)
     
     # generate correct laguerre polynomial
     laguerre_polynomial = genlaguerre(radial_order, azimuthal_order, monic = False)
@@ -57,11 +84,6 @@ def laguerre_gauss(x, y, z, wavelength = 532e-9, waist_radius = 100e-6, radial_o
         for iy in range (0, np.shape(y)[0]):
             rho = np.sqrt(x[ix]**2 + y[iy]**2)
             phi = np.arctan2(y[iy], x[ix])
-            w = waist_radius * np.sqrt(1 + (z / zR)**2)
-            if (z != 0):
-                R = z * (1 + (zR / z)**2)
-            elif (z == 0):
-                R = np.inf
             term1 = 1 / w
             term2 = ((rho * np.sqrt(2)) / (w))**np.abs(azimuthal_order)
             term3 = np.exp(-(rho**2) / (w**2))
@@ -71,7 +93,7 @@ def laguerre_gauss(x, y, z, wavelength = 532e-9, waist_radius = 100e-6, radial_o
             term7 = np.exp(1j * (np.abs(azimuthal_order) + 2 * radial_order + 1) * np.arctan(z/zR))
             lg_beam[ix, iy] = term1 * term2 * term3 * term4 * term5 * term6 * term7
     lg_beam /= np.max(np.abs(lg_beam))
-    return lg_beam, w, R, zR
+    return lg_beam
 
 
     
